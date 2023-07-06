d0a8c6e0faee3e24e7f79144bc6238cb734ddc7c
KarimAED <karim.alaael-din@gmx.de>
2023-07-06 15:33:50 +0100

    fix: avoid circular import

--- dqc/xc/custom_xc.py
+++ dqc/xc/custom_xc.py
@@ -1,7 +1,6 @@
 from abc import abstractmethod, abstractproperty
 from typing import Union, List
 import torch
-from dqc.api.getxc import get_xc
 from dqc.xc.base_xc import BaseXC
 from dqc.utils.datastruct import ValGrad, SpinParam
 
@@ -24,22 +23,3 @@ class CustomXC(BaseXC, torch.nn.Module):
             return names
         else:
             return super().getparamnames(methodname, prefix=prefix)
-
-
-class BasicCustomXC(CustomXC):
-    """basic xc model joining components for x and c"""
-
-    def __init__(self, models: list[str]):
-        assert all(
-            [m[: m.find("_")] == models[0][: models[0].find("_")] for m in models]
-        ), "base models must belong to same family"
-        super().__init__()
-        self.models = [get_xc(m) for m in models]
-        assert all([m.family is not None and m.family >= 0 for m in self.models])
-
-    @property
-    def family(self) -> int:
-        return self.models[0].family
-
-    def get_edensityxc(self, densinfo) -> torch.Tensor:
-        return torch.stack([m.get_edensityxc(densinfo) for m in self.models]).sum(0)
