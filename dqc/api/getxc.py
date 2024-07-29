a80e7ba34a6e9a628fb64a8dc038a31c835fb874
KarimAED <karim@aedin.dev>
2024-07-29 15:03:55 +0100

    wip: added pyproject.toml temporarily

--- dqc/api/getxc.py
+++ dqc/api/getxc.py
@@ -35,11 +35,33 @@ def get_xc(xc_names: str | List[str]) -> BaseXC:
         # Map the different components of the xc to the right functions
         components = [comp.strip() for comp in xc_names.split("+")]
 
-        new_xcstr = " + ".join(
-            comp if comp.startswith("fn_")
-            else f"get_libxc('{comp}')"
-            for comp in components
-        )
+        formatted_components = []
+
+        def is_number(str_inp):
+            try:
+                float(str_inp)
+                return True
+            except ValueError:
+                return False
+
+        def map_libxc_or_fnxc(str_inp):
+            if str_inp.startswith("fn_"):
+                return str_inp
+            return f"get_libxc('{str_inp}')"
+
+        for comp in components:
+            if "*" in comp:
+                comp_parts = comp.split("*")
+                comp_parts = [
+                    cp if is_number(cp.strip())
+                    else map_libxc_or_fnxc(cp)
+                    for cp in comp_parts
+                ]
+                formatted_components.append("*".join(comp_parts))
+            else:
+                formatted_components.append(comp)
+
+        new_xcstr = " + ".join(formatted_components)
 
         # evaluate the expression and return the xc
         glob = {"get_libxc": get_libxc, "fn_linear": function_xc.get_linear}
