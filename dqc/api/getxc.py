a8305aab577d6e9108ca4afe65680b6846f209f5
Karim <karim@aedin.dev>
2024-07-30 13:37:30 +0100

    Feat/arbitrary fn xc (#6)
    * feat: support for arbitrary fn xc functionals
    
    * fix: function_xc init
    
    * fix: getxc
    
    * fix: getxc
    
    * wip: added pyproject.toml temporarily
    
    * fix: whitespace bug
    
    * fix: another bug relating to getxc

--- dqc/api/getxc.py
+++ dqc/api/getxc.py
@@ -9,6 +9,7 @@ except (ImportError, ModuleNotFoundError) as e:
     warnings.warn("Failed to import pylibxc. Might not be able to use xc.")
 from dqc.xc.base_xc import BaseXC, ZeroXC
 from dqc.xc.libxc import get_libxc
+from dqc.xc import function_xc
 
 __all__ = ["get_xc"]
 
@@ -30,12 +31,40 @@ def get_xc(xc_names: str | List[str]) -> BaseXC:
     if not xc_names:
         return ZeroXC()
     elif isinstance(xc_names, str):
-        # wrap the name of xc with "get_libxc"
-        pattern = r"([a-zA-Z_$][a-zA-Z_$0-9]*)"
-        new_xcstr = re.sub(pattern, r'get_libxc("\1")', xc_names)
+
+        # Map the different components of the xc to the right functions
+        components = [comp.strip() for comp in xc_names.split("+")]
+
+        formatted_components = []
+
+        def is_number(str_inp):
+            try:
+                float(str_inp)
+                return True
+            except ValueError:
+                return False
+
+        def map_libxc_or_fnxc(str_inp):
+            if str_inp.startswith("fn_"):
+                return str_inp
+            return f"get_libxc('{str_inp}')"
+
+        for comp in components:
+            if "*" in comp:
+                comp_parts = comp.split("*")
+                comp_parts = [
+                    cp if is_number(cp.strip())
+                    else map_libxc_or_fnxc(cp.strip())
+                    for cp in comp_parts
+                ]
+                formatted_components.append("*".join(comp_parts))
+            else:
+                formatted_components.append(map_libxc_or_fnxc(comp))
+
+        new_xcstr = " + ".join(formatted_components)
 
         # evaluate the expression and return the xc
-        glob = {"get_libxc": get_libxc}
+        glob = {"get_libxc": get_libxc, "fn_linear": function_xc.get_linear}
         return eval(new_xcstr, glob)
     else:
         xc_sum = get_libxc(xc_names[0])
